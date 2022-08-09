68385eb0b9fb3f647e652fa617be6ffa8df7253f
sarvar <sarich97@icloud.com>
2022-08-09 10:56:07 +0700

    create register page

--- app/src/main/java/hamsafar/tj/fragments/RegisterFragment.java
+++ app/src/main/java/hamsafar/tj/fragments/RegisterFragment.java
@@ -1,5 +1,6 @@
 package hamsafar.tj.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import hamsafar.tj.R;
+import hamsafar.tj.activity.RegisterActivity;
 
 public class RegisterFragment extends Fragment {
 
@@ -61,7 +63,11 @@ public class RegisterFragment extends Fragment {
                     userRegisterBtn.setVisibility(View.VISIBLE);
                 } else
                 {
-
+                    Intent registerIntent = new Intent(getContext(), RegisterActivity.class);
+                    registerIntent.putExtra("userEmal", email);
+                    registerIntent.putExtra("userName", name);
+                    registerIntent.putExtra("userPass", password);
+                    startActivity(registerIntent);
                 }
             }
         });
