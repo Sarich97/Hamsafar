79b5b913d9f355a77a37162f0ef6ab58adf0faa5
sarvar <sarich97@icloud.com>
2022-08-11 15:13:02 +0700

    Переделка регистрации в новый дизайн page

--- app/src/main/java/hamsafar/tj/activity/AuthActivity.java
+++ app/src/main/java/hamsafar/tj/activity/AuthActivity.java
@@ -1,23 +1,26 @@
 package hamsafar.tj.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import hamsafar.tj.R;
-import hamsafar.tj.adapters.TabAdapter;
 
 public class AuthActivity extends AppCompatActivity {
 
-
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager2;
-    private TabAdapter tabAdapter;
+    private EditText userEmail, userName, userPass; // Поля ввод юзера
+    private Button userRegisterBtn; // Кнопка регистрации
+    private ProgressBar registerProgress; // Прогрессбар страница(фрагмент) регистрации
+    private TextView singInUser; // Пользовательское соглашение
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,39 +28,62 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // Full Screen Page
 
-        tabLayout = findViewById(R.id.tabLayout);
-        viewPager2 = findViewById(R.id.viewPager);
+
+        userEmail = findViewById(R.id.user_Email);
+        userName = findViewById(R.id.user_Name);
+        userPass = findViewById(R.id.user_Pass);
+        userRegisterBtn = findViewById(R.id.registerNextBtn);
+        registerProgress = findViewById(R.id.progressBarAuth);
+        singInUser = findViewById(R.id.singIN);
 
 
-        tabLayout.addTab(tabLayout.newTab().setText("Вход"));
-        tabLayout.addTab(tabLayout.newTab().setText("Регистрация"));
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        tabAdapter = new TabAdapter(fragmentManager , getLifecycle());
-        viewPager2.setAdapter(tabAdapter);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        singInUser.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager2.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(AuthActivity.this, R.style.BottomSheetDialogTheme);
+                bottomSheetDialog.setContentView(R.layout.sing_in_sheet);
+                bottomSheetDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+                bottomSheetDialog.show();
             }
         });
 
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+        userRegisterBtn.setOnClickListener(new View.OnClickListener() {  // Нажатие на кнопки регистрации
             @Override
-            public void onPageSelected(int position) {
-                tabLayout.selectTab(tabLayout.getTabAt(position));
+            public void onClick(View view) {
+                registerProgress.setVisibility(View.VISIBLE);
+                userRegisterBtn.setVisibility(View.INVISIBLE);
+                String email = userEmail.getText().toString();
+                String name = userName.getText().toString();
+                String password = userPass.getText().toString();
+
+                if(email.length() < 5)
+                {
+                    userEmail.setError("Обязательное поле и не менее 6 символов");
+                    registerProgress.setVisibility(View.INVISIBLE);
+                    userRegisterBtn.setVisibility(View.VISIBLE);
+                }
+                else if(name.length() < 3)
+                {
+                    userName.setError("Обязательное поле и не менее 3 символов");
+                    registerProgress.setVisibility(View.INVISIBLE);
+                    userRegisterBtn.setVisibility(View.VISIBLE);
+
+                } else if(password.length() < 6)
+                {
+                    userPass.setError("Обязательное поле и не менее 6 символов");
+                    registerProgress.setVisibility(View.INVISIBLE);
+                    userRegisterBtn.setVisibility(View.VISIBLE);
+
+                } else
+                {
+                    Intent registerIntent = new Intent(AuthActivity.this, RegisterActivity.class);
+                    registerIntent.putExtra("userEmail", email);
+                    registerIntent.putExtra("userName", name);
+                    registerIntent.putExtra("userPass", password);
+                    startActivity(registerIntent);
+                }
             }
         });
+
     }
 }
