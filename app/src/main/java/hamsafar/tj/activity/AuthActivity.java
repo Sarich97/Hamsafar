4a06a138ce4e2bc10e011c05b04f20d0216c9095
sarvar <sarich97@icloud.com>
2022-08-11 16:08:07 +0700

    Переделка регистрации в новый дизайн page

--- app/src/main/java/hamsafar/tj/activity/AuthActivity.java
+++ app/src/main/java/hamsafar/tj/activity/AuthActivity.java
@@ -1,17 +1,25 @@
 package hamsafar.tj.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import hamsafar.tj.R;
 
@@ -22,12 +30,18 @@ public class AuthActivity extends AppCompatActivity {
     private ProgressBar registerProgress; // Прогрессбар страница(фрагмент) регистрации
     private TextView singInUser; // Пользовательское соглашение
 
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // Full Screen Page
 
+        firebaseAuth = FirebaseAuth.getInstance();  // User Table variable
+        firebaseFirestore = FirebaseFirestore.getInstance();  // DataBase variable
 
         userEmail = findViewById(R.id.user_Email);
         userName = findViewById(R.id.user_Name);
@@ -37,13 +51,43 @@ public class AuthActivity extends AppCompatActivity {
         singInUser = findViewById(R.id.singIN);
 
 
-        singInUser.setOnClickListener(new View.OnClickListener() {
+        singInUser.setOnClickListener(new View.OnClickListener() { // КНОПКА ПОКАЗ ОКОШКИ ВХОДА
             @Override
             public void onClick(View view) {
                 BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(AuthActivity.this, R.style.BottomSheetDialogTheme);
                 bottomSheetDialog.setContentView(R.layout.sing_in_sheet);
                 bottomSheetDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
                 bottomSheetDialog.show();
+
+
+                TextView userEmilSingIn = bottomSheetDialog.findViewById(R.id.userEmailSingIn);
+                TextView userPassSingIn = bottomSheetDialog.findViewById(R.id.userPassSingIn);
+                Button singInUser = bottomSheetDialog.findViewById(R.id.loginBtn);
+                ProgressBar singInProgressBar = bottomSheetDialog.findViewById(R.id.progressBarLogin);
+
+                singInUser.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String email = userEmilSingIn.getText().toString();
+                        String pass = userPassSingIn.getText().toString();
+                        singInUser.setVisibility(View.INVISIBLE);
+                        singInProgressBar.setVisibility(View.VISIBLE);
+
+                        if(TextUtils.isEmpty(email)) {
+                            userEmail.setError("Обязательное поле");
+                            singInUser.setVisibility(View.VISIBLE);
+                            singInProgressBar.setVisibility(View.INVISIBLE);
+                        } else  if(TextUtils.isEmpty(pass)) {
+                            userPass.setError("Обязательное поле");
+                            singInUser.setVisibility(View.VISIBLE);
+                            singInProgressBar.setVisibility(View.INVISIBLE);
+                        } else  {
+                            singInUser.setVisibility(View.VISIBLE);
+                            singInProgressBar.setVisibility(View.INVISIBLE);
+                            loginUser(email, pass);
+                        }
+                    }
+                });
             }
         });
 
@@ -85,5 +129,20 @@ public class AuthActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void loginUser(String email, String pass) {
+        firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Intent mainIntent = new Intent(AuthActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                } else {
+                    Toast.makeText(AuthActivity.this, "Ошибка входа в аккаунт, повторите попытку позже #23", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
