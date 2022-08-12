a50c1c737bd45e509bb51ce197164ca80408c434
sarvar <sarich97@icloud.com>
2022-08-12 09:14:45 +0700

    Оптимизация и доработка что-то(уже не помню) Создал страницу CreatPostActivity.java

---
+++ app/src/main/java/hamsafar/tj/activity/CreatPostActivity.java
@@ -0,0 +1,16 @@
+package hamsafar.tj.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hamsafar.tj.R;
+
+public class CreatPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creat_post);
+    }
+}
--- app/src/main/java/hamsafar/tj/activity/MainActivity.java
+++ app/src/main/java/hamsafar/tj/activity/MainActivity.java
@@ -6,11 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import hamsafar.tj.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    private  Button button;
+    private ImageView userProfileBtn;
+
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +28,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        userProfileBtn = findViewById(R.id.user_ProgileBtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();  // User Table variable
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AuthActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        userProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentUser !=null) {
+
+                } else {
+                    Intent authIntent = new Intent(MainActivity.this, AuthActivity.class);
+                    startActivity(authIntent);
+                    finish();
+                }
             }
         });
     }
