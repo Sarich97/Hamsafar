9f3aa5d8bed4a4439b4a681c52547e36d1cb84b3
Sarvar Sultoonov <alemdar.1997@inbox.ru>
2022-08-14 13:08:57 +0700

    Работа над дизайн поста, но пока польное говно, но вроде уже что-то не плохо. Нужно доработать, а то получается пока не очень и добавить кол-во мест в поездке

--- app/src/main/java/hamsafar/tj/activity/MainActivity.java
+++ app/src/main/java/hamsafar/tj/activity/MainActivity.java
@@ -1,15 +1,23 @@
 package hamsafar.tj.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import hamsafar.tj.R;
@@ -17,6 +25,8 @@ import hamsafar.tj.R;
 public class MainActivity extends AppCompatActivity {
     private  Button button;
     private ImageView userProfileBtn;
+
+    private String userID;
 
 
     private FirebaseAuth firebaseAuth;
@@ -26,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         button = findViewById(R.id.button);
         userProfileBtn = findViewById(R.id.user_ProgileBtn);
@@ -46,6 +57,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(currentUser !=null) {
+                    BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this, R.style.BottomSheetDialogTheme);
+                    bottomSheetDialog.setContentView(R.layout.user_profile_sheet);
+                    bottomSheetDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+                    bottomSheetDialog.show();
+
+                    TextView userName = bottomSheetDialog.findViewById(R.id.user_Name);
+                    TextView carModel = bottomSheetDialog.findViewById(R.id.car_Model);
+                    TextView userPhone = bottomSheetDialog.findViewById(R.id.user_Phone);
+                    TextView userEmail = bottomSheetDialog.findViewById(R.id.user_Email);
+
+                    userID = firebaseAuth.getCurrentUser().getUid();
+                    firebaseFirestore.collection("users").document(userID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if(task.isSuccessful()) {
+                                if(task.getResult().exists()) {
+                                    String user_name = task.getResult().getString("userName");
+                                    String user_email = task.getResult().getString("userEmail");
+                                    String user_phone = task.getResult().getString("userPhone");
+                                    String car_model = task.getResult().getString("userCarModel");
+
+                                    String firstName = user_name.substring(0, 1);
+
+                                    userName.setText(user_name);
+                                    userEmail.setText(user_email);
+                                    userPhone.setText("+992" + user_phone);
+                                    carModel.setText(car_model);
+                                }
+                            }
+                        }
+                    });
 
                 } else {
                     Intent authIntent = new Intent(MainActivity.this, AuthActivity.class);
