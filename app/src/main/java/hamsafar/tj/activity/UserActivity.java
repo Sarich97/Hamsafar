d1b67faf1093afea25b09cb9465dc4057cf021f0
sarvar <sarich97@icloud.com>
2022-08-22 10:38:16 +0700

    Make userprofile page

---
+++ app/src/main/java/hamsafar/tj/activity/UserActivity.java
@@ -0,0 +1,16 @@
+package hamsafar.tj.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hamsafar.tj.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
